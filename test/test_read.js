183a5246090e4d13b9fe147fa14a46d54670699a
NicksComingUp <niclaus.a@gmail.com>
2018-08-16 17:10:08 -0500

    query modifiers skip, limit, sort in action

--- test/test_read.js
+++ test/test_read.js
@@ -1,10 +1,15 @@
 const assert = require('assert');
 const User = require('../src/user');
 describe('Reading Users out of the database', ()=> {
-  let amanda
+  let amanda, sherill, abby, nemo;
   beforeEach(async () => {
-    amanda = new User({ name : 'Amanda'});
-    await amanda.save();
+    amanda  = new User({ name : 'Amanda'});
+    sherill = new User({ name : 'Sherill'});
+    abby    = new User({ name : 'Abby'});
+    nemo    = new User({ name : 'Nemo'});
+
+    //save operations in parallel
+    await Promise.all([amanda.save() ,sherill.save(), abby.save(), nemo.save()]);
   });
   it('finds all users with a name of joe', async () => {
     const found = await User.find({name: 'Amanda'});
@@ -13,5 +18,15 @@ describe('Reading Users out of the database', ()=> {
   it('finds all users with a unique id', async () => {
     const found = await User.findOne({_id: amanda._id});
    assert(found.name.toString() === amanda.name.toString());
+  });
+
+  it('can skip and limit the result set',async ()=>{
+      const users = await User.find({})
+      .sort({ name: 1})
+      .skip(1)
+      .limit(2);
+      assert(users.length === 2);
+      assert(users[0]['name'] === 'Amanda');
+      assert(users[1]['name'] === 'Nemo');
   });
 });
