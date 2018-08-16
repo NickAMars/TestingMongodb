87cb7e5f883cf0004aa57e33f3f20cdf001e6363
NicksComingUp <niclaus.a@gmail.com>
2018-08-16 17:07:43 -0500

    dropping all of the previous existing database

--- test/test_helper.js
+++ test/test_helper.js
@@ -9,10 +9,23 @@ before( (done) => {
 });
 
 // define the mongoose connection before this
-beforeEach(  (done) =>{
-  mongoose.connection.collections.users.drop(()=>{
-    done();
+beforeEach( (done) => {
+  /*
+  mongo normalizes each collection name
+   by lower casing the entire collection
+   console.log(mongoose.connection.collections);
+   */
+  const { users, comments , blogposts } = mongoose.connection.collections;
+  // console.log(users);
+  users.drop(()=>{
+    comments.drop(()=>{
+      blogposts.drop(()=>{
+        done();
+      });
+    });
   });
+
 });
+
 
 //https://medium.com/@danmolitor/writing-tests-for-mongo-mongoose-with-mocha-7e98be740074
